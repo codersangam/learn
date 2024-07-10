250ea8477b4364a376dd6082cce6e9f5d0e2296e
Sangam Singh <hritriik@gmail.com>
2024-07-11 00:11:03 +0400

    Initial Commit

---
+++ learn/learnApp.swift
@@ -0,0 +1,17 @@
+//
+//  learnApp.swift
+//  learn
+//
+//  Created by Sangam Singh on 11/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct learnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
